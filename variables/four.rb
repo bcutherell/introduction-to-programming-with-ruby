e22e7e001928551b51506d05e09e131f69d412d5
Blake Cutherell <bcutherell@gmail.com>
2023-11-08 13:21:51 -0600

    variable exercises

---
+++ variables/four.rb
@@ -0,0 +1,9 @@
+puts "Tell me your first name!"
+
+first_name = gets.chomp
+
+puts "Tell me your last name!"
+
+last_name = gets.chomp
+
+puts "Hello #{first_name} #{last_name}!"
--- variables/three.rb
+++ variables/three.rb
@@ -3,3 +3,7 @@ puts "Tell me your name!"
 name = gets.chomp
 
 puts "Hello #{name}!"
+
+10.times do 
+	puts name
+end
